b4b36ed3394f86217553026027850b8b3238cf2a
Clownworldenjoyer76 <mlames@protonmail.com>
2026-02-23 14:27:26 -0500

    Update name_normalization.py

--- docs/win/hockey/scripts/00_parsing/name_normalization.py
+++ docs/win/hockey/scripts/00_parsing/name_normalization.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-# docs/win/soccer/scripts/00_parsing/name_normalization.py
-
+#docs/win/hockey/scripts/00_parsing/name_normalization.py
 import csv
 from pathlib import Path
 from datetime import datetime
