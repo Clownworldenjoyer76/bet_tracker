91e9552fb0ca24a7812a6295a420974278786e34
Clownworldenjoyer76 <mlames@protonmail.com>
2026-03-06 18:33:31 -0500

    Update config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -14,9 +14,7 @@ window.REPO_CONFIG = {
     sport:"basketball",
     isHockey:false,
     selectFiles:(date)=>[
-      `win/basketball/04_select/${date}_basketball_NBA_moneyline.csv`,
-      `win/basketball/04_select/${date}_basketball_NBA_spread.csv`,
-      `win/basketball/04_select/${date}_basketball_NBA_total.csv`
+      `win/basketball/04_select/daily_slate/${date}_nba.csv`
     ],
     predFile:(date)=>`win/basketball/00_intake/predictions/basketball_NBA_${date}.csv`,
     bookFile:(date)=>`win/basketball/00_intake/sportsbook/basketball_NBA_${date}.csv`
@@ -26,9 +24,7 @@ window.REPO_CONFIG = {
     sport:"basketball",
     isHockey:false,
     selectFiles:(date)=>[
-      `win/basketball/04_select/${date}_basketball_NCAAB_moneyline.csv`,
-      `win/basketball/04_select/${date}_basketball_NCAAB_spread.csv`,
-      `win/basketball/04_select/${date}_basketball_NCAAB_total.csv`
+      `win/basketball/04_select/daily_slate/${date}_ncaab.csv`
     ],
     predFile:(date)=>`win/basketball/00_intake/predictions/basketball_NCAAB_${date}.csv`,
     bookFile:(date)=>`win/basketball/00_intake/sportsbook/basketball_NCAAB_${date}.csv`
