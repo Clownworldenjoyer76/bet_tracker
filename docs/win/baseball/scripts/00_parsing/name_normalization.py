50f1e9a6e888f6951d559a8431545eb1ae39e0ad
Clownworldenjoyer76 <mlames@protonmail.com>
2026-04-09 11:14:01 -0400

    Update name_normalization.py

--- docs/win/baseball/scripts/00_parsing/name_normalization.py
+++ docs/win/baseball/scripts/00_parsing/name_normalization.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# docs/win/baseball/scripts/00_parsing/name_normalization.py
+# docs/win/baseball/scripts/00_intake/name_normalization.py
 
 import csv
 from pathlib import Path
