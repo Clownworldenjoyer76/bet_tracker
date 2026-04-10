24c17f059e2a564c884034a0b54999e46530022e
Clownworldenjoyer76 <mlames@protonmail.com>
2026-04-10 12:18:25 -0400

    Update apply_run_line_juice.py

--- docs/win/baseball/scripts/02_juice/apply_run_line_juice.py
+++ docs/win/baseball/scripts/02_juice/apply_run_line_juice.py
@@ -130,9 +130,9 @@ def main():
     }
     per_file = []
 
-    for f in OUTPUT_DIR.glob("*.csv"):
+    for f in OUTPUT_DIR.glob("*run_line.csv"):
         f.unlink()
-        
+
     try:
         _log(f"INPUT_DIR : {INPUT_DIR}")
         _log(f"JUICE_FILE: {JUICE_FILE}")
