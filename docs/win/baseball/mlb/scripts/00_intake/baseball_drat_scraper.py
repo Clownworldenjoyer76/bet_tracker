ce0f0a556c37fd1922d91596939ec40e8fda46d3
Clownworldenjoyer76 <mlames@protonmail.com>
2026-06-17 12:31:28 -0400

    Update baseball_drat_scraper.py

--- docs/win/baseball/mlb/scripts/00_intake/baseball_drat_scraper.py
+++ docs/win/baseball/mlb/scripts/00_intake/baseball_drat_scraper.py
@@ -11,9 +11,13 @@ from datetime import datetime
 import pytz
 from playwright.sync_api import sync_playwright
 
+#URLS = {
+#    "mlb": "https://www.dratings.com/predictor/mlb-baseball-predictions/",
+#}
 URLS = {
-    "mlb": "https://www.dratings.com/predictor/mlb-baseball-predictions/",
+    "mlb": "https://www.dratings.com/predictor/mlb-baseball-predictions/upcoming/2#scroll-upcoming",
 }
+
 
 UTC = pytz.utc
 ET = pytz.timezone("America/New_York")
